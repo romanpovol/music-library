0a3107ea3b73abf0d34cf102a7680f73bdd0dc12
romanpovol <roman.a.p@bk.ru>
2024-11-27 00:44:40 +0300

    init logger

--- cmd/music_library/main.go
+++ cmd/music_library/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/romanpovol/music_library/internal/config"
+
+	"log"
+	"log/slog"
 )
 
 func main() {
@@ -11,11 +15,49 @@ func main() {
 
 	fmt.Println(*cfg)
 
-	// TODO: init logger
+	log := setupLogger(cfg.LogLevel)
+	log.Info(
+		"starting server",
+		slog.String("server", cfg.HTTPServer.Address),
+	)
 
 	// TODO: init storage
 
 	// TODO: init router
 
 	// TODO: run server
+}
+
+const (
+	LevelDebug = "Debug"
+	LevelInfo  = "Info"
+	LevelWarn  = "Warn"
+	LevelError = "Error"
+)
+
+func setupLogger(logLevel string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch logLevel {
+	case LevelDebug:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case LevelInfo:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	case LevelWarn:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelWarn}),
+		)
+	case LevelError:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError}),
+		)
+	default:
+		log.Fatalf("unexpected log level %s", logLevel)
+	}
+
+	return logger
 }
